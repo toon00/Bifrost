73bf934e7b5cdf827f9b3618a96db0147c32c8d5
toon00 laptop <toon00+laptop@gmail.com>
2019-01-29 21:48:40 +0100

    Added swagger endpoint

--- Bifrost.Samples/GpioSwitcherWebApi/Startup.cs
+++ Bifrost.Samples/GpioSwitcherWebApi/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace GpioSwitcherWebApi
 {
@@ -17,12 +18,22 @@ namespace GpioSwitcherWebApi
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+             {
+                 c.SwaggerDoc("v1", new Info { Title = "PinApi", Version = "v1" });
+             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
